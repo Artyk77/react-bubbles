01731ca6fe086410eda7ebc1a9b69cd7cc40bfda
jose restrepo <arturo.restrepo@gmail.com>
2019-08-23 11:51:09 -0500

    MVP completed

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +7,8 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  
+  console.log(colorToEdit);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,10 +22,31 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-  };
+  
+
+  axiosWithAuth()
+  .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+  .then(res => {
+    const newColors = colors.map(color => {
+      if (color.id === colorToEdit.id) {
+        return res.data;
+      }
+      return color;
+    });
+    updateColors(newColors)
+  })
+  .catch(error => console.log(error))
+};
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        const newColors = colors.filter(c => c.id !== color.id)
+        updateColors(newColors)
+      })
+      .catch(error => console.log(error))
   };
 
   return (
@@ -32,12 +54,14 @@ const ColorList = ({ colors, updateColors }) => {
       <p>colors</p>
       <ul>
         {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
+           <li key={color.color}>
             <span>
               <span className="delete" onClick={() => deleteColor(color)}>
                 x
               </span>{" "}
-              {color.color}
+              <span onClick={() => editColor(color)}>
+                {color.color}
+              </span>
             </span>
             <div
               className="color-box"
@@ -71,7 +95,7 @@ const ColorList = ({ colors, updateColors }) => {
             />
           </label>
           <div className="button-row">
-            <button type="submit">save</button>
+          <button onClick={saveEdit}type="submit">save</button>
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
@@ -79,7 +103,8 @@ const ColorList = ({ colors, updateColors }) => {
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
     </div>
-  );
-};
+      );
+            };
+
 
 export default ColorList;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import axios from 'axios'
+import axios from 'axios';
 
-const Login = () => {
   const Login = (props) => {
     const [user, setUser] = useState({username: '', password: ''})
 
